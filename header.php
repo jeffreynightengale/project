dd36eb2d6fd661c287585b8dcec9ac1c82eb901d
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-21 16:53:41 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RHJ Movies</title>
+  </head>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="employees.php">Employees</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="managers.php">Managers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="supervisors.php">Supervisors</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<body>
