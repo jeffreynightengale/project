6fc033c849b335671987d074204541754dec81be
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-11-28 14:08:33 -0600

    Update directions.php

--- directions.php
+++ directions.php
@@ -1,5 +1,6 @@
 <?php require_once("header.php"); ?>
 
 <h1 class="text-center">Here are the directions to our theater!</h1>
+</br>
 
 <iframe class="text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d417264.7363693845!2d-97.6855249631168!3d35.20943914721566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b26b9b3533a49f%3A0x7dec3c01307946ce!2sRegal%20Warren%20Moore!5e0!3m2!1sen!2sus!4v1669665882920!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
