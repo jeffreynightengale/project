e8e67f248b0bf3d144ead63a589f2dc736eea1bd
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-12-03 14:34:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="text-center">
   <img src="RHJ.png" class="rounded">
 </div>
-<p>Welcome to RHJ Movie Theater! This website shows our movies playing, menu, and how to get to our theater.</p>
+<p class="text-bg-primary p-3">Welcome to RHJ Movie Theater! This website shows our movies playing, menu, and how to get to our theater.</p>
 <div class="text-center">
   <h1>Featured Movie</h1>
 </div>
