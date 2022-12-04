372f013d0e0c65e069ffb3e0fd4f83388437fce5
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-04 17:32:57 -0600

    Update movie.php

--- movie.php
+++ movie.php
@@ -1,6 +1,4 @@
 <?php require_once("header.php"); ?>
-
-
   <body class="text-center">
     <h2>Movies Showing</h2>
   <?php
@@ -41,7 +39,6 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </card-group>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
