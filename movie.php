4d2ba4447c9cb1e9d1fe54423f374534fe04f58d
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-04 17:38:46 -0600

    Update movie.php

--- movie.php
+++ movie.php
@@ -24,7 +24,7 @@ if ($result->num_rows > 0) {
     <div class="row row-cols-6 row-cols-md-3 g-4">
     <div class="col">
 
-     <div id="card" class="card"  style="width: 15rem; height: 30rem;">
+     <div id="card" class="card"  style="height: 30rem;">
          <img  src=<?=$row["Image"]?> class="card-img-top" alt="...">
   <div  class="card-body">
     <a class="card-title" href="movie-details.php?id=<?=$row["movieID"]?>"><?=$row["Title"]?></a
