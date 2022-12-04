aca7aee0bd3638f52971142774ab2a39e34a9561
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-04 16:59:24 -0600

    Update directions.php

--- directions.php
+++ directions.php
@@ -1,9 +1,9 @@
 <?php require_once("header.php"); ?>
 
 <h1 class="text-center">Here are the directions to our theater!</h1>
+<div class="text-center"> Once you exit I-35 on SW 19th street there are two ways to get our theater!  You can take a right on I-35 service road after you cross the bridge on SW 19th street and its a straight shot from there. </div>
+<div class="text-center"> Or you can turn right on Telephone Rd and continue on Telephone Rd until you hit SW 11th street then take a right and enter the parking lot!</div>
 <h1> </h1>
 <div class="position-absolute top-50 start-50 translate-middle">
 <iframe class="text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d417264.7363693845!2d-97.6855249631168!3d35.20943914721566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b26b9b3533a49f%3A0x7dec3c01307946ce!2sRegal%20Warren%20Moore!5e0!3m2!1sen!2sus!4v1669665882920!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
-<div class="text-center"> Once you exit I-35 on SW 19th street there are two ways to get our theater!  You can take a right on I-35 service road after you cross the bridge on SW 19th street and its a straight shot from there. </div>
-<div class="text-center"> Or you can turn right on Telephone Rd and continue on Telephone Rd until you hit SW 11th street then take a right and enter the parking lot!</div>
