1e0caa04803a16693c24d4bf573704f089cb1ec3
hhersley <89473870+hhersley@users.noreply.github.com>
2022-12-05 13:38:42 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+      <link rel="stylesheet" href="StyleSheet2.css" />
+  <body>
+    <footer>
+      <p> follow us on our social media!
+      
+      <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+<a href="https://twitter.com/?lang=en" class="fa fa-twitter"></a>
+        <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+        <a href="https://www.linkedin.com/" class="fa fa-linkedin"></a>
+      </p>
+    </footer>
+  </body>
