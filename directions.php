25cfa1895c88a4c3d8e257e07e38a7dfcbdc812b
hhersley <89473870+hhersley@users.noreply.github.com>
2022-12-05 13:41:29 -0600

    Update directions.php

--- directions.php
+++ directions.php
@@ -5,3 +5,4 @@
 <div class="position-absolute top-50 start-50 translate-middle">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d417264.7363693845!2d-97.6855249631168!3d35.20943914721566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b26b9b3533a49f%3A0x7dec3c01307946ce!2sRegal%20Warren%20Moore!5e0!3m2!1sen!2sus!4v1669665882920!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
+    <?php require_once('footer.php'); ?>
