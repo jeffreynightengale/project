4cdc3ad0df977ff08f143dadb717e0714250b12e
hhersley <89473870+hhersley@users.noreply.github.com>
2022-12-07 14:01:06 -0600

    Create header-admin.php

---
+++ header-admin.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RHJ Movies</title>
+  </head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="movie-admin.php">Movie</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="menu-admin.php">Menu</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="ticket-admin.php">Ticket</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="reward-admin.php">Reward</a>
+        </li>
+      </ul>
+    </div>
+          <img src="RHJ1.png" class="rounded float-end" width="85" height="65">
+  </div>
+</nav>
+<body>
