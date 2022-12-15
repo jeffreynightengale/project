1893a130ce921572d1a919e7c5c36337429d8303
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-15 12:03:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if ($conn->connect_error) {
 ?>
  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                   <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                   </ol>
