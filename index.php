5c33bbf1d6e9a50ef706ccb9f27da90ffaaabd87
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-14 18:50:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -34,10 +34,32 @@ if ($conn->connect_error) {
 </div>
 <p class="text-bg-primary p-3 text-center">Welcome to RHJ Movie Theater! This website shows our movies playing, menu, and how to get to our theater.</p>
 <div class="text-center">
-  <h1>Featured Movie:</h1>
+  <h1>Featured Movies:</h1>
 </div>
-<div class="text-center">
-  <img src="https://m.media-amazon.com/images/M/MV5BNTM4NjIxNmEtYWE5NS00NDczLTkyNWQtYThhNmQyZGQzMjM0XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UX1000_.jpg" style="height: 30vw;" class="rounded">
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="la.jpg" alt="Los Angeles">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
 </div>
 
     
