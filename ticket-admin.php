6cdf7229047090cdac021e80bc8cd3a3b0bab846
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-15 13:21:45 -0600

    Update ticket-admin.php

--- ticket-admin.php
+++ ticket-admin.php
@@ -15,180 +15,57 @@ if ($conn->connect_error) {
      if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
        case 'Add':
-      $sqlAdd = "insert into Movie (Title, Starring, Director, Duration, Summary, Image, Trailer) value (?, ?, ?, ?, ?, ?, ?)";
+      $sqlAdd = "insert into Ticket (Showtime, movieID, Seat, memberID, foodID) value (?, ?, ?, ?, ?)";
       $stmtAdd = $conn->prepare($sqlAdd);
-    $stmtAdd->bind_param("sssssss", $_POST['mTitle'], $_POST['mStarring'], $_POST['mDirector'], $_POST['mDuration'], $_POST['mSummary'], $_POST['mImage'], $_POST['mTrailer']);
+    $stmtAdd->bind_param("ss", $_POST['tShowtime'], $_POST['tMovieid'], $_POST['tSeat'], $_POST['tMemberid'], $_POST['tFoodid']);
     $stmtAdd->execute();
-      echo '<div class="alert alert-success" role="alert">Movie added.</div>';
-      break;
-    case 'Edit':
-      $sqlEdit = "update Movie set Title=?, Starring=?, Director=?, Duration=?, Summary=?, Image=?, Trailer=? where movieID=?";
-      $stmtEdit = $conn->prepare($sqlEdit);
-      $stmtEdit->bind_param("sssssssi", $_POST['mTitle'], $_POST['mStarring'], $_POST['mDirector'], $_POST['mDuration'], $_POST['mSummary'], $_POST['mImage'], $_POST['mTrailer'], $_POST['mid']);
-      $stmtEdit->execute();
-      echo '<div class="alert alert-success" role="alert">Movie edited.</div>';
-      break;
-    case 'Delete':
-      $sqlDelete = "delete from Movie where MovieID=?";
-      $stmtDelete = $conn->prepare($sqlDelete);
-      $stmtDelete->bind_param("i", $_POST['mid']);
-      $stmtDelete->execute();
-      echo '<div class="alert alert-success" role="alert">Movie deleted.</div>';
+      echo '<div class="alert alert-success" role="alert">Ticket now in system.</div>';
       break;
   }
      }
     ?>
     
-      <h2>Employee</h2>   
-<table class="table table-striped">
-  <thead>
-    <tr>
-  
-            <th> Title</th>
-            <th> Starring </th>
-            <th> Director </th>
-            <th> Duration </th>
-            <th> Summary</th>
-       <th> Image </th>
-            <th> Trailer</th>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
- 
-$sql = "Select movieID, Title, Image, Starring, Director, Duration, Summary, Trailer from Movie";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["Title"]?></td>
-    <td><?=$row["Starring"]?></td>
-       <td><?=$row["Director"]?></td>
-        <td><?=$row["Duration"]?></td>
-                <td><?=$row["Summary"]?></td>
-                    <td><?=$row["Image"]?></td>
-                <td><?=$row["Trailer"]?></td>
- <td>
-              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editMovie<?=$row["movieID"]?>">
-                Edit
-              </button>
-              <div class="modal fade" id="editMovie<?=$row["movieID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editMovie<?=$row["movieID"]?>Label" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="editMovie<?=$row["movieID"]?>Label">Edit Movie</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <form method="post" action="">
-                        <div class="mb-3">
-                          <label for="editMovie<?=$row["movieID"]?>Name" class="form-label">Title</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Name" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mTitle" value="<?=$row['Title']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Enter the new title.</div>
-                          <div class="mb-3">
-                           <label for="editMovie<?=$row["movieID"]?>Starring" class="form-label">Starring</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Name" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mStarring" value="<?=$row['Starring']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Add actors.</div>
-                             <div class="mb-3">
-                          <label for="editMovie<?=$row["movieID"]?>Director" class="form-label">Director</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Director" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mDirector" value="<?=$row['Director']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Enter the new director.</div>
-                          <div class="mb-3">
-                           <label for="editMovie<?=$row["movieID"]?>Duration" class="form-label">Duration</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Duration" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mDuration" value="<?=$row['Duration']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Edit duration.</div>
-                             <div class="mb-3">
-                          <label for="editMovie<?=$row["movieID"]?>Summary" class="form-label">Summary</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Summary" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mSummary" value="<?=$row['Summary']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Enter the new summary.</div>
-                          <div class="mb-3">
-                           <label for="editMovie<?=$row["movieID"]?>Image" class="form-label">Image</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Image" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mImage" value="<?=$row['Image']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Add image url.</div>
-                            <label for="editMovie<?=$row["movieID"]?>Trailer" class="form-label">Trailer</label>
-                          <input type="text" class="form-control" id="editMovie<?=$row["movieID"]?>Name" aria-describedby="editMovie<?=$row["movieID"]?>Help" name="mTrailer" value="<?=$row['Trailer']?>">
-                          <div id="editMovie<?=$row["movieID"]?>Help" class="form-text">Add trailer.</div>
-                            <input type="hidden" name="mid" value="<?=$row['movieID']?>">
-                        <input type="hidden" name="saveType" value="Edit">
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                            </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td>
-              <form method="post" action="">
-                <input type="hidden" name="mid" value="<?=$row["movieID"]?>" />
-                <input type="hidden" name="saveType" value="Delete">
-                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
-              </form>
-            </td>
-          </tr>
-   <?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
-     </tbody>
+      <h2>Employee</h2>  
+    </tbody>
       </table>
       <br />
       <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addMovie">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addTicket">
         Add New
       </button>
 
       <!-- Modal -->
-      <div class="modal fade" id="addMovie" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addMovieLabel" aria-hidden="true">
+      <div class="modal fade" id="addTicket" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addTicketLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="addMovieLabel">Add Movie</h1>
+              <h1 class="modal-title fs-5" id="addTicketLabel">Add Ticket</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <form method="post" action="">
                 <div class="mb-3">
-                  <label for="movieTitle" class="form-label">Movie Title</label>
-                  <input type="text" class="form-control" id="movieTitle" aria-describedby="nameHelp" name="mTitle">
-                  <div id="nameHelp" class="form-text">Enter the movie's title.</div>
+                  <label for="ticketShowtime" class="form-label">Showtime</label>
+                  <input type="text" class="form-control" id="ticketShowtime" aria-describedby="nameHelp" name="tShowtime">
+                  <div id="nameHelp" class="form-text">Enter the showtime.</div>
                 </div>
                 <div class="mb-3">
-                  <label for="movieStarring" class="form-label">Movie Starring</label>
-                  <input type="text" class="form-control" id="movieStarring" aria-describedby="nameHelp" name="mStarring">
-                  <div id="nameHelp" class="form-text">Enter the movie's cast.</div>
+                  <label for="movieID" class="form-label">Movie ID</label>
+                  <input type="text" class="form-control" id="movieID" aria-describedby="nameHelp" name="tMovieid">
+                  <div id="nameHelp" class="form-text">Enter the movie ID.</div>
+                </div>
+                <label for="ticketSeat" class="form-label">Seat</label>
+                  <input type="text" class="form-control" id="ticketSeat" aria-describedby="nameHelp" name="tSeat">
+                  <div id="nameHelp" class="form-text">Enter the seat.</div>
                 </div>
                 <div class="mb-3">
-                  <label for="movieDirector" class="form-label">Movie Director</label>
-                  <input type="text" class="form-control" id="movieDirector" aria-describedby="nameHelp" name="mDirector">
-                  <div id="nameHelp" class="form-text">Enter the movie's director.</div>
+                  <label for="memberID" class="form-label">Member ID</label>
+                  <input type="text" class="form-control" id="memberID" aria-describedby="nameHelp" name="tMemberid">
+                  <div id="nameHelp" class="form-text">Enter the member ID.</div>
                 </div>
-                <div class="mb-3">
-                  <label for="movieDuration" class="form-label">Movie Duration</label>
-                  <input type="text" class="form-control" id="movieDuration" aria-describedby="nameHelp" name="mDuration">
-                  <div id="nameHelp" class="form-text">Enter the movie's duration (ex. #h #m).</div>
-                </div>
-                <div class="mb-3">
-                  <label for="movieSummary" class="form-label">Movie Summary</label>
-                  <input type="text" class="form-control" id="movieSummary" aria-describedby="nameHelp" name="mSummary">
-                  <div id="nameHelp" class="form-text">Enter the movie's summary.</div>
-                </div>
-                <div class="mb-3">
-                  <label for="movieImage" class="form-label">Movie Image</label>
-                  <input type="text" class="form-control" id="movieImage" aria-describedby="nameHelp" name="mImage">
-                  <div id="nameHelp" class="form-text">Enter the movie's picture.</div>
-                </div>
-                <div class="mb-3">
-                  <label for="movieTrailer" class="form-label">Movie Trailer</label>
-                  <input type="text" class="form-control" id="movieTrailer" aria-describedby="nameHelp" name="mTrailer">
-                  <div id="nameHelp" class="form-text">Enter the movie's trailer.</div>
+              <label for="foodID" class="form-label">Food ID</label>
+                  <input type="text" class="form-control" id="foodID" aria-describedby="nameHelp" name="tFoodid">
+                  <div id="nameHelp" class="form-text">Enter the food ID.</div>
                 </div>
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -198,7 +75,37 @@ $conn->close();
         </div>
       </div>
     </div>
-   <?php require_once("footer.php"); ?>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Showtime</th>
+            <th>Seat</th>
+            <th>Member ID</th>
+      <th>Movie ID</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+ 
+$sql = "Select * from Ticket";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["Showtime"]?></td>
+    <td><?=$row["Seat"]?></td>
+       <td><?=$row["memberID"]?></td>
+    <td><?=$row["movieID"]?></td>
+   <?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
