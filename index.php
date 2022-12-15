57a307a8b5538491488dc08ab1683e753f93e6a8
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-14 19:07:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -46,16 +46,9 @@ if ($result->num_rows > 0) {
 ?>
 <div id="demo" class="carousel slide" data-ride="carousel">
 
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-
   <!-- The slideshow -->
   <div class="carousel-inner">
-    <div class="carousel-item active">
+    <div class="carousel-item">
       <img src=<?=$row["Image"]?>>
     </div>
   </div>
