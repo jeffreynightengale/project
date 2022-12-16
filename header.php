17896de1e5a548640c1dab29433cb7d364e20eb1
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-15 21:42:55 -0600

    Update header.php

--- header.php
+++ header.php
@@ -22,11 +22,11 @@
         <li class="nav-item">
           <a class="nav-link" href="menu.php">Menu</a>
         </li>
-         <li class="nav-item">
-          <a class="nav-link" href="admin.php">Admin</a>
-        </li>
         <li class="nav-item">
           <a class="nav-link" href="directions.php">Directions</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="admin.php">Admin</a>
         </li>
       </ul>
     </div>
