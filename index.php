b789c2035832cdbf62dfd6835fcba0572d374104
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-15 21:45:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -77,7 +77,8 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
-  
+<?$conn->close();
+?>
 
 ?>
     <div class="text-center">
@@ -121,6 +122,4 @@ if ($result->num_rows > 0) {
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 </body>
-<?$conn->close();
-?>
     <?php require_once('footer.php'); ?>
