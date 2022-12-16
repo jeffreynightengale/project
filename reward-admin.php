4adf73ef1439b999c31eebd3c948ba0e1eb763e4
hhersley <89473870+hhersley@users.noreply.github.com>
2022-12-15 22:13:55 -0600

    Update reward-admin.php

--- reward-admin.php
+++ reward-admin.php
@@ -21,6 +21,20 @@ if ($conn->connect_error) {
     $stmtAdd->execute();
       echo '<div class="alert alert-success" role="alert">Member added.</div>';
       break;
+         case 'Edit':
+      $sqlEdit = "update Reward set Email=?, Name=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("ss", $_POST['mEmail'], $_POST['mName']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Member edited.</div>';
+      break;
+        case 'Delete':
+      $sqlDelete = "delete from Reward where memberID=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['mid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Member deleted.</div>';
+      break;
   }
      }
     ?>
@@ -84,13 +98,63 @@ if ($result->num_rows > 0) {
     <td><?=$row["memberID"]?></td>
     <td><?=$row["Email"]?></td>
        <td><?=$row["Name"]?></td>
+      <td>
+                  <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editReward<?=$row["memberID"]?>">
+                    Edit
+                  </button>
+                  <div class="modal fade" id="editReward<?=$row["memberID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editReward<?=$row["memberID"]?>Label" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="editReward<?=$row["memberID"]?>Label">Edit Member</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <form method="post" action="">
+                            <div class="mb-3">
+                              <label for="editReward<?=$row["memberID"]?>Name" class="form-label">Member Name</label>
+                              <input type="text" class="form-control" id="editReward<?=$row["memberID"]?>Name" aria-describedby="editReward<?=$row["memberID"]?>Help" name="mName" value="<?=$row['Name']?>">
+                              <div id="editReward<?=$row["memberID"]?>Help" class="form-text">Enter the member's name.</div>
+                            </div>
+                           <div class="mb-3">
+                              <label for="editReward<?=$row["memberID"]?>Email" class="form-label">Email</label>
+                              <input type="text" class="form-control" id="editReward<?=$row["memberID"]?>Email" aria-describedby="editReward<?=$row["memberID"]?>Help" name="mEmail" value="<?=$row['Email']?>">
+                              <div id="editReward<?=$row["memberID"]?>Help" class="form-text">Enter the member's email.</div>
+                            </div>
+    
+    
+                            <input type="hidden" name="mid" value="<?=$row['memberID']?>">
+                            <input type="hidden" name="saveType" value="Edit">
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                </td>
+    
+    
+    
+    
+     <td>
+              <form method="post" action="">
+                <input type="hidden" name="mid" value="<?=$row["memberID"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+    </tr>
+  
    <?php
   }
 } else {
   echo "0 results";
 }
 $conn->close();
+   
 ?>
+  </tbody>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
