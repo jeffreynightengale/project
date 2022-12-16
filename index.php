4281376648dc3c16f054e3f7a7ae3afac5ef5cab
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-12-15 21:49:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -42,13 +42,13 @@ if ($conn->connect_error) {
  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://m.media-amazon.com/images/M/MV5BYzZkOGUwMzMtMTgyNS00YjFlLTg5NzYtZTE3Y2E5YTA5NWIyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg" class="d-block w-100" alt="...">
+      <img src="https://m.media-amazon.com/images/M/MV5BYzZkOGUwMzMtMTgyNS00YjFlLTg5NzYtZTE3Y2E5YTA5NWIyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg" class="d-block w-50" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://lumiere-a.akamaihd.net/v1/images/sandlot_584x800_us_56dada88.jpeg" class="d-block w-100" alt="...">
+      <img src="https://lumiere-a.akamaihd.net/v1/images/sandlot_584x800_us_56dada88.jpeg" class="d-block w-50" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg" class="d-block w-100" alt="...">
+      <img src="https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg" class="d-block w-50" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
